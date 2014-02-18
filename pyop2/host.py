b7c5e169fc87471966b42ab0b7710440dd771f29
Gheorghe-Teodor Bercea <gb308@doc.ic.ac.uk>
2014-02-18 10:42:57 +0000

    Add POSIX timing to generated host code

--- pyop2/host.py
+++ pyop2/host.py
@@ -661,6 +661,31 @@ class JITModule(base.JITModule):
                 externc_open = 'extern "C" {'
                 externc_close = '}'
         headers = "\n".join([compiler.get('vect_header', ""), blas_header])
+
+        self.timer_function = """
+        #include <time.h>
+        long stamp()
+        {
+          clockid_t id;
+          struct timespec ts;
+#if _POSIX_CPUTIME > 0
+          /* Clock ids vary by OS.  Query the id, if possible. */
+          if ( clock_getcpuclockid( 0, &id ) == -1 )
+#endif
+#if defined(CLOCK_PROCESS_CPUTIME_ID)
+            /* Use known clock id for AIX, Linux, or Solaris. */
+            id = CLOCK_PROCESS_CPUTIME_ID;
+#elif defined(CLOCK_VIRTUAL)
+            /* Use known clock id for BSD or HP-UX. */
+            id = CLOCK_VIRTUAL;
+#else
+            id = (clockid_t)-1;
+#endif
+          if ( id != (clockid_t)-1 && clock_gettime( id, &ts ) != -1 )
+            return (double)ts.tv_sec + (double)ts.tv_nsec / 1000000000.0;
+          return -1; /* Failed */
+          }
+        """
         if any(arg._is_soa for arg in self._args):
             kernel_code = """
             #define OP2_STRIDE(a, idx) a[idx]
