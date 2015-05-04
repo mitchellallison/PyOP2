fc719e7f3f4d6b00295225717cbb4398d00231a5
Mitchell Allison <mka211@doc.ic.ac.uk>
2015-05-04 13:44:01 +0100

    Adding both lazy and greedy profiling.

--- test/profile.sh
+++ test/profile.sh
@@ -12,15 +12,18 @@ else
   backends=(sequential opencl opencl)
   profile_names=(Sequential OpenCL_CPU OpenCL_GPU)
   machines=(edge02 edge02 graphic06)
+  execution_types=(greedy lazy)
 
   for i in "${!backends[@]}"; do
-    backend=${backends[$i]}
-    profile_name=${profile_names[$i]}
-    machine=${machines[$i]}
-    selected_test="$test_name[$backend-greedy-$discretisation-$mesh_size-$layer_size]"
-    echo "### Profiling $profile_name, using backend $backend on $machine ###"
-    for (( i=0; i < $iterations; i++ )); do
-      echo "PYOPENCL_CTX=0 py.test -v /homes/mka211/Documents/IndividualProject/PyOP2/test/unit/test_opencl_extrusion.py --backend=$backend -k $selected_test --profile=$profile_name -sx" | ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $machine bash -l;
+    for execution_type in "${execution_types[@]}"; do
+      backend=${backends[$i]}
+      profile_name=${profile_names[$i]}
+      machine=${machines[$i]}
+      selected_test="$test_name[$backend-$execution_type-$discretisation-$mesh_size-$layer_size]"
+      echo "### Profiling $profile_name, using backend $backend on $machine ###"
+      for (( i=0; i < $iterations; i++ )); do
+        echo "PYOPENCL_CTX=0 py.test -v /homes/mka211/Documents/IndividualProject/PyOP2/test/unit/test_opencl_extrusion.py --backend=$backend -k $selected_test --profile=$profile_name -sx" | ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $machine bash -l;
+      done
     done
   done
 fi
