e3cad617c9208989efaff159d864584feda03332
Mitchell Allison <mitchellallison@me.com>
2015-05-06 13:32:17 +0100

    Changes to plotter.

--- test/plot.py
+++ test/plot.py
@@ -17,13 +17,16 @@ graph_dir = os.path.abspath(sys.argv[2])
 
 data = pandas.read_csv(input_file)
 
-grouped_profile_data = data.groupby('Profile')
-
 # With runtime
-figure = grouped_profile_data.mean().plot(kind='bar', stacked=True).get_figure()
-figure.savefig(os.path.join(graph_dir, "{}_runtime_comparison.pdf".format(file_name)))
+grouped_profile_data = data.groupby('Profile')
+runtime_figure = grouped_profile_data.mean().plot(kind='barh', stacked=True).get_figure()
+runtime_figure.set_size_inches(20, 10)
+runtime_figure.savefig(os.path.join(graph_dir, "{}_runtime_comparison.pdf".format(file_name)))
+
+plt.figure()
 
 # Without runtime
-parloop_profile_data = data['ParLoop kernel'].groupby('Profile')
-figure = parloop_profile_data.mean().plot(kind='bar', stacked=True).get_figure()
-figure.savefig(os.path.join(graph_dir, "{}_parloop_comparison.pdf".format(file_name)))
+parloop_profile_data = data.groupby('Profile')
+parloop_figure = parloop_profile_data['ParLoop kernel'].mean().plot(kind='barh', stacked=False).get_figure()
+parloop_figure.set_size_inches(20, 10)
+parloop_figure.savefig(os.path.join(graph_dir, "{}_parloop_comparison.pdf".format(file_name)))
