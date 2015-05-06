c8148ffe9237b486d63a9715646aca1a91bab1a1
Mitchell Allison <mka211@doc.ic.ac.uk>
2015-05-06 16:53:12 +0100

    Adding a script to run a series of profiling jobs.

---
+++ test/profile_runner.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+discretisations=(CG1 DG1)
+test=test_extruded_rhs_assembly
+layers=(1 2 3 4 8 10 15 30 60)
+mesh_size=100x100
+iterations=10
+
+for disc_1 in "${discretisations[@]}"; do
+  for disc_2 in "${discretisations[@]}"; do
+    for layer in "${layers[@]}"; do
+       ./profile.sh $test $disc_1-$disc_2 $mesh_size $layer $iterations
+    done
+  done
+done
