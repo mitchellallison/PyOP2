342ab492e10eef372fbdc300125fca01302070f6
Mitchell Allison <mka211@doc.ic.ac.uk>
2015-06-02 15:50:10 +0100

    The profile runner will now run execution scheme A and B over opencl backends.

--- test/profile_runner.sh
+++ test/profile_runner.sh
@@ -2,9 +2,9 @@
 
 discretisations=(CG1 DG1)
 test=test_extruded_rhs_assembly
-layers=(1 2 3 4 8 10 15 30 45 60)
-mesh_size=100x100
-iterations=10
+layers=(1 2 4 8 16 32 64 96 128 160 192 224 256)
+mesh_size=square
+iterations=15
 
 for disc_1 in "${discretisations[@]}"; do
   for disc_2 in "${discretisations[@]}"; do
