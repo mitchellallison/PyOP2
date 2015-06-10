0f6ceb3a774f0a373aad2b295ba43262ac0bc436
Mitchell Allison <mka211@doc.ic.ac.uk>
2015-06-10 17:20:51 +0100

    Adding both scheme C and D to the partition tuner.

--- test/partition_size_tuner.sh
+++ test/partition_size_tuner.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-discretisations=(CG1)
+discretisations=(CG1 DG1)
 test=test_extruded_rhs_assembly
-#layers=(1 2 4 8 16 32 64 96 128 160 192 224 256)
-layers=(8 16 32 64 96 128 160 192 224 256)
-#layers=(1 2 4 10 30 50 100)
-partition_sizes=(0 1 2 3 4 5 5.585 6 7 8 9 10 11)
+layers=(1 2 4 8 16 32 64 96 128 160 192 224 256)
+partition_sizes=(2 3 4 5 5.585 6 7 8 9 10)
+execution_schemes=(C D)
 mesh=square
-iterations=1
+iterations=5
 
 for disc_1 in "${discretisations[@]}"; do
   for disc_2 in "${discretisations[@]}"; do
@@ -17,7 +16,11 @@ for disc_1 in "${discretisations[@]}"; do
         partition_size_eval=`printf "%.0f" $(echo "e(l(2)*(10+$partition_size))" | bc -l)`
         echo "### Profiling $mesh mesh with $layer layers with the partition scaled by 1/$partition_size_eval ###"
         for (( i=0; i < $iterations; i++ )); do
-          echo "PYOP2_PROFILING=1 PYOPENCL_CTX=0 PYOP2_PARTITION_SIZE=$partition_size_eval py.test -v /homes/mka211/Documents/IndividualProject/PyOP2/test/unit/test_opencl_extrusion.py --backend=opencl -k $selected_test --profile=$partition_size_eval" | bash
+          command="PYOP2_EXECUTION_SCHEME=0 PYOP2_PROFILING=1 PYOPENCL_CTX=0 PYOP2_PARTITION_SIZE=$partition_size_eval py.test -v /homes/mka211/Documents/IndividualProject/PyOP2/test/unit/test_opencl_extrusion.py --backend=opencl -k $selected_test --profile='$partition_size_eval ${execution_schemes[0]}'"
+          for (( scheme=1; scheme < 2; scheme++ )); do
+            command="$command && PYOP2_EXECUTION_SCHEME=$scheme PYOP2_PROFILING=1 PYOPENCL_CTX=0 PYOP2_PARTITION_SIZE=$partition_size_eval py.test -v /homes/mka211/Documents/IndividualProject/PyOP2/test/unit/test_opencl_extrusion.py --backend=opencl -k $selected_test --profile='$partition_size_eval ${execution_schemes[$scheme]}'"
+          done
+          echo $command | bash
         done
       done
     done
